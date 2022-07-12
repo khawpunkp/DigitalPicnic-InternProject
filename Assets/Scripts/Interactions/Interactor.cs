53a272623d4191fbdb675b2eca9a0718f65268a5
Krittayos Poomthong <krittayos.kp@gmail.com>
2022-07-12 16:04:06 +0700

    build | Interaction Demo

--- Assets/Scripts/Interactions/Interactor.cs
+++ Assets/Scripts/Interactions/Interactor.cs
@@ -1,8 +1,4 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using Photon.Pun;
-using UnityEditor.UIElements;
 using UnityEngine;
 
 public class Interactor : MonoBehaviourPun
