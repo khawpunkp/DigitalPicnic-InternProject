c59a12f23ecfa88656ac04f75245ac888421cba4
kp <krittayos.kp@gmail.com>
2022-07-14 19:09:59 +0700

    feat | Display MainMenu on Awake

---
+++ Assets/Scripts/UI/Lobby/MainMenu/MainMenu.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    private void Awake()
+    {
+        gameObject.SetActive(true);
+    }
+}
