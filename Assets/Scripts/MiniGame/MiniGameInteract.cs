79de26f782802b61fd4a8cfa1ba1699a8e94bf6b
kp <krittayos.kp@gmail.com>
2022-07-19 17:35:43 +0700

    feat | Diasble Player

--- Assets/Scripts/MiniGame/MiniGameInteract.cs
+++ Assets/Scripts/MiniGame/MiniGameInteract.cs
@@ -8,24 +8,28 @@ using UnityEngine;
 public class MiniGameInteract : MonoBehaviour
 {
     private float countDownTime = 0;
-    public float startTime = 5.9f;
+    public float startTime = 5.2f;
+    public bool isActive = false;
+    private GameObject[] players;
+    private GameObject localPlayer;
     [SerializeField] private TextMeshProUGUI time;
+    [SerializeField] private GameObject MiniGame;
 
     private void Start()
     {
         countDownTime = startTime;
         time.gameObject.SetActive(false);
+        MiniGame.SetActive(false);
     }
 
     private void Update()
     {
-        // Debug.Log(countDownTime);
-        // countDownTime -= 1 * Time.deltaTime;
-        // time.text = countDownTime.ToString("0");
-        // if (countDownTime <= 3)
-        //     time.color = Color.red;
-        // if (countDownTime <= 0)
-        //     countDownTime = 0;
+        players = GameObject.FindGameObjectsWithTag("Player");
+        foreach (var p in players)
+        {
+            if (p.GetPhotonView().IsMine)
+                localPlayer = p;
+        }
     }
 
     private void OnTriggerEnter(Collider other)
@@ -36,18 +40,24 @@ public class MiniGameInteract : MonoBehaviour
 
     private void OnTriggerStay(Collider other)
     {
-        Debug.Log(countDownTime);
-        countDownTime -= 1 * Time.deltaTime;
+        isActive = true;
+        MiniGame.SetActive(true);
         time.text = countDownTime.ToString("0");
+        countDownTime -= 1 * Time.deltaTime; 
         if (countDownTime <= 3)
             time.color = Color.red;
         if (countDownTime <= 0)
-            PhotonNetwork.LoadLevel("MiniGame");
+            time.gameObject.SetActive(false);
+        localPlayer.GetComponent<ThirdPersonController>().enabled = false;
+        localPlayer.transform.GetChild(0).gameObject.SetActive(false);
     }
 
     private void OnTriggerExit(Collider other)
     {
+        isActive = false;
         countDownTime = startTime;
         time.gameObject.SetActive(false);
+        MiniGame.SetActive(false);
+        localPlayer.SetActive(true);
     }
 }
